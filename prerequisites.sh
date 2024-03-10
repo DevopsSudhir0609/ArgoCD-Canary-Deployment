5c367232b3cfd983fedb7087f7124bf74456bc0b
Aut0ps <autops8600.gmail.com>
2024-03-11 01:20:54 +0530

    modified prerequisites.sh file

--- prerequisites.sh
+++ prerequisites.sh
@@ -10,21 +10,23 @@ handle_error() {
 echo "Installing Docker..."
 curl -fsSL https://get.docker.com -o get-docker.sh || handle_error "Failed to download Docker"
 sudo sh get-docker.sh || handle_error "Failed to install Docker"
-sudo rm -rf get-docker.sh 
+sudo rm -rf get-docker.sh
+sudo usermod -aG docker $USER
 
 # 2. Install Minikube
 echo "Installing Minikube..."
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 || handle_error "Failed to download Minikube"
 sudo install minikube-linux-amd64 /usr/local/bin/minikube || handle_error "Failed to install Minikube"
 sudo rm -rf minikube-linux-amd64
+minikube start --driver=docker
 
 # 3. Install kubectl
 echo "Installing kubectl..."
-curl -LO https://dl.k8s.io/release/v1.29.0/bin/linux/amd64/kubectl || handle_error "Failed to download kubectl"
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256" || handle_error "Failed to download kubectl sha256"
-echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check || handle_error "Failed to verify kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl || handle_error "Failed to install kubectl"
-sudo rm -rf kubectl && kubectl version --client || handle_error "Failed to verify kubectl version"
+curl -LO https://dl.k8s.io/release/v1.29.0/bin/linux/amd64/kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo rm -rf kubectl && kubectl version --client
 
 # 4. Install ArgoCD
 echo "Installing ArgoCD..."
